8d4574398e38d50e49ece2a27bf4475db7298c98
robbfreitas <robertocompromissos@gmail.com>
2022-02-02 18:37:01 -0300

    Comportamento das classes

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 class Cliente {
     nome;
     cpf;
+
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    };
 }
 
 const cliente1 = new Cliente();
-
 cliente1.nome = "Ricado";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
 
 console.log(cliente1);
