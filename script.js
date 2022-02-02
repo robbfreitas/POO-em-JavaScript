e631e5c6e0e4f8ad85be858afee60cc42972f7b0
robbfreitas <robertocompromissos@gmail.com>
2022-02-02 19:09:29 -0300

    Sobre atributos privados

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo =0 ;
 
     sacar(valor){
-        if(this.saldo >= valor) {
-            this.saldo -= valor;
+        if(this._saldo >= valor) {
+            this._saldo -= valor;
         }
-    };
+    }
+
+    depositar(valor) {
+        if(valor > 0) {
+            this._saldo += valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -20,7 +26,9 @@ cliente1.nome = "Ricado";
 cliente1.cpf = 11122233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
-console.log(cliente1);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo);
