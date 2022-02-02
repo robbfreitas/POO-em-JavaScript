33a54f547ea464fa991e35444da607e4f1218601
robbfreitas <robertocompromissos@gmail.com>
2022-02-02 18:25:51 -0300

    Primeira versão de classe

---
+++ script.js
@@ -0,0 +1,16 @@
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+
+cliente1.nome = "Ricado";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+
+console.log(cliente1);
