18e7c9a2bb3f7b45fc8851fa0b91c09ff035c8e7
robbfreitas <robertocompromissos@gmail.com>
2022-02-14 11:34:13 -0300

    getters e setters

--- script.js
+++ script.js
@@ -15,11 +15,9 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente();
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf = 88822233309;
+conta2.cliente = cliente1   ;
 conta2.agencia = 102;
 
-contaCorrenteRicardo.transferir(200, conta2);
-
-console.log(conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
+console.log(conta2.saldo);
